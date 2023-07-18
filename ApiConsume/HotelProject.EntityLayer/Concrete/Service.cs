b24396ea12a0f1ee3e41a6e0a3f73967a708dce1
Haktan <Haktan@HAKTAN>
2023-07-18 03:32:57 +0300

    Add project files.

---
+++ ApiConsume/HotelProject.EntityLayer/Concrete/Service.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelProject.EntityLayer.Concrete
+{
+    public class Service
+    {
+        public int ServiceId { get; set; }
+        public string ServiceIcon { get; set; }
+        public string Title { get; set; }
+        public string Decription { get; set; }
+    }
+}
---
+++ ApiConsume/HotelProject.EntityLayer/Concrete/Testimonial.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelProject.EntityLayer.Concrete
+{
+    public class Testimonial
+    {
+        public int TestimonialId { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Decription { get; set; }
+        public string Image { get; set; }
+    }
+}
