f0a2931b36d1a51018ff4742aa72497285ec3056
haktanbasak <haktanbasak9@gmail.com>
2023-12-10 04:30:22 +0300

    Rezervasyon sayfası eklendi

---
+++ ApiConsume/HotelProject.DataAccessLayer/Migrations/20231210011117_mig_add_booking.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HotelProject.DataAccessLayer.Migrations
+{
+    public partial class mig_add_booking : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Mail",
+                table: "Bookings",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Mail",
+                table: "Bookings");
+        }
+    }
+}
